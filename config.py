ad1415a39ed6bc9c06a602e1f21cff99bd9f3750
maximilianmozes <mozesmaj@gmail.com>
2024-05-06 17:24:26 +0100

    fix config

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
 
         assert language in ["nl", "en"], f"Invalid language {language} specified (only 'nl' and 'en' supported)"
         
-        self.path_to_model = f"./data/models/{language}"
+        self.path_to_model = f"./data/{language}"
         self.model_type = "bert" if language == "nl" else "roberta"
