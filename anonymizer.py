b6c2c524e2a84ba6a3fee667df4d9e1672361359
Stefan Kirsch <113800603+StefKirsch@users.noreply.github.com>
2025-06-04 10:31:13 +0200

    Fix URL replacement bug in anonymize function

--- anonymizer.py
+++ anonymizer.py
@@ -234,7 +234,7 @@ class Anonymizer:
         anon_input_seq = re.sub("https*://\S+", "URL", orig_input_seq)
 
         anon_input_seq = self.replace_identified_entities(
-            entities, orig_input_seq, entity2generic
+            entities, anon_input_seq, entity2generic
         )
 
         anon_input_seq = self.replace_numerics(anon_input_seq)
