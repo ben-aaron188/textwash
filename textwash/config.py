914f70f8e6514162fc32482f0c836ce8b2ed9d2a
Stefan Kirsch <stefankirsch@posteo.de>
2025-06-17 14:42:40 +0200

    Move data folder into top-level package folder
    This will make it easier to include and access the data when the code is installed as a package.

--- textwash/config.py
+++ textwash/config.py
@@ -1,9 +1,9 @@
 class Config:
     def __init__(self, language):
-        self.path_to_months_file = "./data/months.txt"
-        self.path_to_written_numbers_file = "./data/written_numbers.txt"
+        self.path_to_months_file = "textwash/data/months.txt"
+        self.path_to_written_numbers_file = "textwash/data/written_numbers.txt"
 
         assert language in ["nl", "en"], f"Invalid language {language} specified (only 'nl' and 'en' supported)"
         
-        self.path_to_model = f"./data/{language}"
+        self.path_to_model = f"textwash/data/{language}"
         self.model_type = "bert" if language == "nl" else "roberta"
